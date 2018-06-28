a2fe4118c3c97a32587683096c5131fc6303a5c4
Li Labas <li.labas1@gmail.com>
2018-06-28 18:26:53 +0200

    Add installer for GCS MEAN stack server by Bitnami

---
+++ scripts/google_cloud_mean_stack_bitnami/Instructions.md
@@ -0,0 +1,40 @@
+#Google Cloud MEAN stack (Bitnami) LND instalation
+This script is tested on Google Cloud Services MEAN stack by Bitnami
+
+## Getting Started
+
+This is a full installer! It will install full bitcoin node, a lightning node and lightning-charge which is a lightning API.
+
+### Prerequisites
+
+You need to have MEAN stack server by Bitnami deployed on Google Cloud Services, with port 9735 open.
+
+##Instalation
+Simply follow these steps
+
+First step
+
+```
+Edit the values in config file (This is for your Lightning Node)
+```
+
+Second step
+
+```
+Install by running ./install.sh
+```
+
+####Now wait for about 12+ hours for bitcoin node to fully sync.
+
+To check bitcoin node syncing progress use
+
+```
+tail -f ~/.bitcoin/debug.log
+```
+
+After bitcoin node is fully synced use this command to start your lightning node and lightning charge
+
+```
+immortal lightningd
+immortal charged --api-token mySecretToken --db-path /home/bitnami/chargedb/charge.db
+```
---
+++ scripts/google_cloud_mean_stack_bitnami/install.sh
@@ -0,0 +1,64 @@
+#install script for debian based systems
+#ufw installation
+cd ~
+sudo apt-get update
+sudo apt-get install ufw
+sudo ufw disable
+sudo ufw default deny incoming 
+sudo ufw default allow outgoing 
+sudo ufw allow ssh  
+sudo ufw allow http 
+sudo ufw allow https 
+sudo ufw allow 9735/tcp 
+sudo ufw allow 9735/udp 
+sudo ufw allow 9735 
+sudo ufw enable
+#bitcoin core installation
+mkdir bitcoinbin
+cd bitcoinbin 
+wget https://bitcoin.org/bin/bitcoin-core-0.16.1/bitcoin-0.16.1-x86_64-linux-gnu.tar.gz 
+tar -xzf bitcoin-0.16.1-x86_64-linux-gnu.tar.gz
+cd ~
+#immortal installation
+curl -s https://packagecloud.io/install/repositories/immortal/immortal/script.deb.sh | sudo bash 
+sudo apt-get install immortal 
+#lightning installation
+sudo apt-get update 
+sudo apt-get install -y autoconf automake build-essential git libtool libgmp-dev libsqlite3-dev python python3 net-tools zlib1g-dev
+mkdir builds
+cd builds
+git clone https://github.com/ElementsProject/lightning.git
+cd lightning
+./configure
+make
+cd ~
+# symlink config
+export PATH=$PATH:~/bitcoinbin/bitcoin-0.16.1/bin/bitcoind
+export PATH=$PATH:~/bitcoinbin/bitcoin-0.16.1/bin/bitcoin-cli
+export PATH=$PATH:~/builds/lightning/lightningd
+export PATH=$PATH:~/builds/lightning/cli
+cd /usr/bin/
+sudo ln -s ~/bitcoinbin/bitcoin-0.16.1/bin/bitcoind bitcoind
+sudo ln -s ~/bitcoinbin/bitcoin-0.16.1/bin/bitcoin-cli bitcoin-cli
+sudo ln -s ~/builds/lightning/lightningd/lightningd lightningd
+sudo ln -s ~/builds/lightning/cli/lightning-cli lightning-cli
+source ~/.bashrc
+source ~/.profile
+cd /opt/bitnami/nodejs/lib
+sudo npm i -g npm
+sudo npm install -g forever
+sudo npm install -g lightning-charge
+cd ~
+mkdir lightning-logs
+mkdir chargedb
+mkdir .lightning
+cp ./config ~/.lightning/
+immortal bitcoind --daemon
+# wait 12+ hours for bitcoin node to fully sync
+
+
+
+#crontab -e
+#@reboot immortal bitcoind --daemon
+#@reboot immortal lightningd
+#@reboot immortal charged --api-token mySecretToken --db-path /home/bitnami/chargedb/charge.db
