fb6fd981d2c60955213c5a56420b187b11e44a0a
Li Labas <li.labas1@gmail.com>
2018-07-02 14:23:27 +0200

    Add output logs

--- scripts/debian-9/install-charge.sh
+++ scripts/debian-9/install-charge.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+NC='\033[0m'
+echo -e "${GREEN}Installing lightning-charge...${NC}"
 cd /usr/lib
 sudo npm install lightning-charge
+echo -e "Installation complete...${NC}"
