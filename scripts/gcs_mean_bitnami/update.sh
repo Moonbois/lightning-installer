5bbdd67ef288735dd17ed47a8e7b6ed24b905ef7
Li Labas <li.labas1@gmail.com>
2021-01-25 20:51:16 +0100

    Update bitcoin core to 0.21.0

--- scripts/gcs_mean_bitnami/update.sh
+++ scripts/gcs_mean_bitnami/update.sh
@@ -12,8 +12,8 @@ cd ~
 rm -rf bitcoinbin/
 mkdir bitcoinbin
 cd bitcoinbin 
-wget https://bitcoin.org/bin/bitcoin-core-0.20.0/bitcoin-0.20.0-x86_64-linux-gnu.tar.gz
-tar -xzf bitcoin-0.20.0-x86_64-linux-gnu.tar.gz
+wget https://bitcoin.org/bin/bitcoin-core-0.21.0/bitcoin-0.21.0-x86_64-linux-gnu.tar.gz
+tar -xzf bitcoin-0.21.0-x86_64-linux-gnu.tar.gz
 cd ~
 immortal bitcoind --daemon
 immortal lightningd
